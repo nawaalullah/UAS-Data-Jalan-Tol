be69d57bf5c6888f9486779db2162eb2de5a571d
MSandiStev <muhammadsandi1002@gmail.com>
2025-07-18 17:23:14 +0700

    remember me done

--- login.php
+++ login.php
@@ -2,6 +2,20 @@
 session_start();
 include 'db.php';
 
+// Cek jika sudah login, arahkan ke index
+if (isset($_SESSION['login'])) {
+    header("Location: index.php");
+    exit();
+}
+
+// Cek cookie jika ada
+if (isset($_COOKIE['username'])) {
+    $_SESSION['login'] = true;
+    $_SESSION['username'] = $_COOKIE['username'];
+    header("Location: index.php");
+    exit();
+}
+
 if (isset($_POST['login'])) {
     $username = $_POST['username'];
     $password = $_POST['password'];
@@ -12,8 +26,14 @@ if (isset($_POST['login'])) {
     if ($user && password_verify($password, $user['password'])) {
         $_SESSION['login'] = true;
         $_SESSION['username'] = $username;
-        setcookie("username", $username, time() + 3600);
+
+        // Jika centang remember me, simpan ke cookie selama 7 hari
+        if (isset($_POST['remember'])) {
+            setcookie("username", $username, time() + (7 * 24 * 60 * 60), "/"); // 7 hari
+        }
+
         header("Location: index.php");
+        exit();
     } else {
         echo "Login gagal!";
     }
@@ -23,5 +43,8 @@ if (isset($_POST['login'])) {
 <form method="POST">
     Username: <input type="text" name="username" required><br>
     Password: <input type="password" name="password" required><br>
+    <label>
+        <input type="checkbox" name="remember"> Remember Me
+    </label><br>
     <button name="login">Login</button>
 </form>
--- logout.php
+++ logout.php
@@ -1,6 +1,9 @@
 <?php
 session_start();
 session_destroy();
-setcookie("username", "", time() - 3600);
+
+// Hapus cookie username
+setcookie("username", "", time() - 3600, "/");
+
 header("Location: login.php");
-?>
+exit();
