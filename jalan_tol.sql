c38f600734d692733a7862bc00004b542af13995
TuruTara2 <andikamrridho@gmail@gmail.com>
2025-07-20 15:52:33 +0700

    ajasendiri

--- jalan_tol.sql
+++ jalan_tol.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 5.2.0
+-- version 5.2.1
 -- https://www.phpmyadmin.net/
 --
--- Host: localhost:3306
--- Generation Time: Jul 20, 2025 at 06:02 AM
--- Server version: 8.0.40
--- PHP Version: 8.1.10
+-- Host: 127.0.0.1:3307
+-- Waktu pembuatan: 20 Jul 2025 pada 10.49
+-- Versi server: 10.4.32-MariaDB
+-- Versi PHP: 8.0.30
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -24,79 +24,76 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
--- Table structure for table `tol`
+-- Struktur dari tabel `tol`
 --
 
 CREATE TABLE `tol` (
-  `id` int NOT NULL,
+  `id` int(11) NOT NULL,
   `tol_asal` varchar(100) DEFAULT NULL,
   `gambar_asal` varchar(255) DEFAULT NULL,
   `tol_tujuan` varchar(100) DEFAULT NULL,
   `gambar_tujuan` varchar(255) DEFAULT NULL,
-  `harga` int DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+  `harga` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
--- Dumping data for table `tol`
+-- Dumping data untuk tabel `tol`
 --
 
 INSERT INTO `tol` (`id`, `tol_asal`, `gambar_asal`, `tol_tujuan`, `gambar_tujuan`, `harga`) VALUES
-(5, 'cileunyi', '1752832624_Screenshot 2025-07-11 225601.png', 'cinunuk', '1752832624_Screenshot 2025-07-16 121643.png', 1000000000),
-(8, 'cibiru', '1752835049_Screenshot 2025-07-17 213636.png', 'pasteur', '1752835049_Screenshot 2025-07-14 172858.png', 50000);
+(1, 'Rancaekek', '1753001181_eldenring.jpg', 'Padalarang', '1753001193_ini dia.jpg', 20000),
+(2, 'Cipularang', '1753000871_dedsec.jpg', 'Cilenyi', '1753000871_eldenring.jpg', 100000);
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `users`
+-- Struktur dari tabel `users`
 --
 
 CREATE TABLE `users` (
-  `id` int NOT NULL,
+  `id` int(11) NOT NULL,
   `username` varchar(50) NOT NULL,
   `password` varchar(255) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
--- Dumping data for table `users`
+-- Dumping data untuk tabel `users`
 --
 
 INSERT INTO `users` (`id`, `username`, `password`) VALUES
-(1, 'sandi', '$2y$10$wEtynfG2m4mc2lr2ghqj2eUHAu53H2I6tRLEvYRCPzjGjK1Y9WZaS'),
-(9, 'dika', '$2y$10$XhoWFuIwm98GaHW2u6xdCukJ8MYk0IBgYNmoQbrsyJfIvAqKe2.1u'),
-(10, 'nawa', '$2y$10$cjOqeL8MgZ0cC6Ea9z91necPIyRBQAGtIrxNIZSSKHflL8py7f1Iy');
+(1, 'andi', '$2y$10$K0Wnv6vYXCMwGAcl5WQOu.qrng3l8FTLBh1cUwDHjuL4GeJAIU6hm');
 
 --
 -- Indexes for dumped tables
 --
 
 --
--- Indexes for table `tol`
+-- Indeks untuk tabel `tol`
 --
 ALTER TABLE `tol`
   ADD PRIMARY KEY (`id`);
 
 --
--- Indexes for table `users`
+-- Indeks untuk tabel `users`
 --
 ALTER TABLE `users`
-  ADD PRIMARY KEY (`id`),
-  ADD UNIQUE KEY `username` (`username`);
+  ADD PRIMARY KEY (`id`);
 
 --
--- AUTO_INCREMENT for dumped tables
+-- AUTO_INCREMENT untuk tabel yang dibuang
 --
 
 --
--- AUTO_INCREMENT for table `tol`
+-- AUTO_INCREMENT untuk tabel `tol`
 --
 ALTER TABLE `tol`
-  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 
 --
--- AUTO_INCREMENT for table `users`
+-- AUTO_INCREMENT untuk tabel `users`
 --
 ALTER TABLE `users`
-  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
